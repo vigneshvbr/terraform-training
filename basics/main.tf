a52848358dcee0ab29b6af05cd2c299a929b54cf
Vignesh S <vigneshvbrs@gmail.com>
2019-08-21 09:05:54 +0000

    Update main.tf to output the container ip and image name while terraform apply

--- basics/main.tf
+++ basics/main.tf
@@ -12,3 +12,15 @@ resource "docker_container" "container_id" {
     external = "80"
   }
 }
+
+#Output the IP Address of the Container
+output "ip_address" {
+  value       = "${docker_container.container_id.ip_address}"
+  description = "The IP for the container."
+}
+
+#Output the Name of the Container
+output "container_name" {
+  value       = "${docker_container.container_id.name}"
+  description = "The name of the container."
+}
