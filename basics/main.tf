80ad4a7b53ba318e80662c82b32238d6c6c41ae7
Vignesh S <vigneshvbrs@gmail.com>
2019-08-21 10:14:51 +0000

    Split main.tf to variables.tf and outputs.tf

--- basics/main.tf
+++ basics/main.tf
@@ -1,24 +1,3 @@
-#Define variables
-variable "image_name" {
-  description = "Image for container."
-  default     = "ghost:latest"
-}
-
-variable "container_name" {
-  description = "Name of the container."
-  default     = "blog"
-}
-
-variable "int_port" {
-  description = "Internal port for container."
-  default     = "2368"
-}
-
-variable "ext_port" {
-  description = "External port for container."
-  default     = "80"
-}
-
 # Download the latest Ghost image
 resource "docker_image" "image_id" {
   name = "${var.image_name}"
@@ -32,16 +11,4 @@ resource "docker_container" "container_id" {
     internal = "${var.int_port}"
     external = "${var.ext_port}"
   }
-}
-
-#Output the IP Address of the Container
-output "ip_address" {
-  value       = "${docker_container.container_id.ip_address}"
-  description = "The IP for the container."
-}
-
-#Output the Name of the Container
-output "container_name" {
-  value       = "${docker_container.container_id.name}"
-  description = "The name of the container."
 }
