45e3aa171343e2869cbd41ebc081ccc023ddc99a
Vignesh S <vigneshvbrs@gmail.com>
2019-12-14 19:34:32 +0000

    use null resource and local_exec

--- basics/main.tf
+++ basics/main.tf
@@ -12,3 +12,9 @@ resource "docker_container" "container_id" {
     external = "${lookup(var.ext_port, var.env)}"
   }
 }
+
+resource "null_resource" "null_id" {
+  provisioner "local-exec" {
+    command = "echo ${docker_container.container_id.name}:${docker_container.container_id.ip_address} >> container.txt"
+  }
+}
